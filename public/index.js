d4e7fd43dcb20d398785793e68ac008eff2712b3
JAHMD <70108576+JAHMD@users.noreply.github.com>
2022-11-24 07:21:05 +0200

    Updated

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
 const menuBtn = document.getElementById("menu-btn");
 const navSection = document.getElementById("navigators-section");
-
+const navWindow = document.getElementById("nav-window");
 document.addEventListener("click", (e) => {
   const closest = e.target.closest("#menu-btn");
   if (e.target !== document.getElementById("menu-icon") || closest === null) {
-    if (navSection.classList.contains("active"))
-      navSection.classList.remove("active");
+    if (navWindow.classList.contains("active"))
+      navWindow.classList.remove("active");
     return;
   }
-  navSection.classList.toggle("active");
+  navWindow.classList.toggle("active");
 });
