d9a6f495fef74d86fd268b93473d5a87ecac294c
JAHMD <70108576+JAHMD@users.noreply.github.com>
2022-11-26 09:56:57 +0200

    adding the footer and adjust the shorten field

--- public/index.js
+++ public/index.js
@@ -1,7 +1,20 @@
 const menuBtn = document.getElementById("menu-btn");
 const navWindow = document.getElementById("nav-window");
+const shortenLinkField = document.getElementById("shorten-link-field");
+const getStartedBtn = document.querySelectorAll(".get-started-btn");
 
 // toggling the nav window
 menuBtn.addEventListener("click", (e) => {
+  const menuIcon = menuBtn.querySelector("i");
+  menuIcon.className = menuIcon.classList.contains("fa-bars")
+    ? "fa-solid fa-xmark"
+    : "fa-solid fa-bars";
   navWindow.classList.toggle("active");
+});
+
+// go the shorten field
+getStartedBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    shortenLinkField.focus();
+  });
 });
