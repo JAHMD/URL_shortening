101dba9a09a9ce3cd2709565932dd682f4183a63
JAHMD <70108576+JAHMD@users.noreply.github.com>
2022-12-03 22:43:39 +0200

    shorten URLs and clear generated links
    make the user able to shorten URLs and adding clear button to clear the generated links

--- public/index.js
+++ public/index.js
@@ -1,7 +1,12 @@
+const BASE_URL = "https://api.shrtco.de/v2/shorten?url=";
 const menuBtn = document.getElementById("menu-btn");
 const navWindow = document.getElementById("nav-window");
-const shortenLinkField = document.getElementById("shorten-link-field");
 const getStartedBtn = document.querySelectorAll(".get-started-btn");
+const shortenLinkField = document.getElementById("shorten-link-field");
+const shortenBtn = document.getElementById("shorten-link-btn");
+const errorMsg = document.getElementById("error-msg");
+const GeneratedLinksList = document.getElementById("generated-links");
+const clearBtn = document.getElementById("clear-btn");
 
 // toggling the nav window
 menuBtn.addEventListener("click", (e) => {
@@ -18,3 +23,89 @@ getStartedBtn.forEach((btn) => {
     shortenLinkField.focus();
   });
 });
+
+// start shorten
+shortenBtn.addEventListener("click", () => {
+  const regex = /(https?:\/\/)?(www\.)?\w+\.\w+.?([\w\W]+)?$/gi;
+  const userLink = shortenLinkField.value;
+  console.log(userLink.match(regex));
+  if (regex.test(userLink)) {
+    const originalLink = BASE_URL + shortenLinkField.value;
+    errorMsg.classList.add("hidden");
+    shortenLinkField.classList.remove("active");
+    getTheShortenLink(originalLink);
+  } else {
+    errorMsg.innerText = `Please add a valid link`;
+    errorMsg.classList.remove("hidden");
+    shortenLinkField.classList.add("active");
+  }
+});
+
+clearBtn.addEventListener("click", () => {
+  GeneratedLinksList.textContent = "";
+});
+
+// functions go here -------------------------------------------
+// fetching the api
+async function fetchData(url) {
+  const response = await fetch(url);
+  console.log(response);
+  const data = await response.json();
+  return data;
+}
+
+// getting the shorten link
+async function getTheShortenLink(url) {
+  shortenBtn.value = "Loading...";
+  const link = await fetchData(url);
+  shortenBtn.value = "Shorten It!";
+  if (link.ok) {
+    errorMsg.classList.add("hidden");
+    shortenLinkField.value = "";
+    shortenLinkField.focus();
+    createLink(link.result);
+    copyLink(link.result);
+  } else {
+    errorMsg.innerText = `You can't shorten this link.`;
+    errorMsg.classList.remove("hidden");
+    shortenLinkField.classList.add("active");
+  }
+}
+
+// create link item
+function createLink(link) {
+  const linkItem = document.createElement("li");
+  linkItem.className = "link-item";
+  //original link
+  const originalLink = document.createElement("p");
+  originalLink.className =
+    "pb-2 md:pb-0 border-b-[1px] md:border-b-0 text-neutral-VeryDarkViolet text-ellipsis overflow-hidden";
+  originalLink.innerText = link.original_link;
+  // shortened link
+  const shortenedLink = document.createElement("a");
+  shortenedLink.className =
+    "generated-link md:ml-auto inline-block text-primary-Cyan text-ellipsis overflow-hidden";
+  shortenedLink.innerText = link.short_link;
+  shortenedLink.href = `https://${link.short_link}`;
+  shortenedLink.target = "_blank";
+  // copy button
+  const copyBtn = document.createElement("button");
+  copyBtn.className = "py-2 px-6 primary rounded-lg";
+  copyBtn.innerText = "Copy";
+  // append the properties to the list item
+  linkItem.append(originalLink, shortenedLink, copyBtn);
+  GeneratedLinksList.prepend(linkItem);
+}
+
+// copy the generated link
+function copyLink(link) {
+  const copyBtns = document.querySelectorAll("#generated-links li button");
+  copyBtns.forEach((btn) => {
+    console.log(copyBtns);
+    btn.addEventListener("click", () => {
+      btn.classList.add("copied");
+      btn.textContent = "Copied!";
+      navigator.clipboard.writeText(link.short_link);
+    });
+  });
+}
